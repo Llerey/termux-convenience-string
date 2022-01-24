cfce26dd7f1c9fa92d6dd55b9181d07cbdb931a3
llerey <llerey.yt@gmail.com>
2022-01-24 22:35:14 +0300

    Update convenience-string.sh

--- convenience-string.sh
+++ convenience-string.sh
@@ -4,6 +4,10 @@ apt install lsd
 
 apt install proot
 
+apt install proot-distro
+
+apt install neofetch
+
 echo alias ap='"apt update && apt upgrade"' >> $HOME/.bashrc
 
 echo alias sd='"cd $HOME/storage/shared && cls"' >> $HOME/.bashrc
@@ -17,6 +21,12 @@ echo alias repo='"termux-change-repo"' >> $HOME/.bashrc
 echo alias cls='"c ; lsd -a"' >> $HOME/.bashrc
 
 echo alias td='"cd $HOME; cls"' >> $HOME/.bashrc
+
+echo alias nf='"neofetch"' >> $HOME/.bashrc
+
+echo alias ubuntu='"cd $HOME ; proot-distro login ubuntu"' >> $HOME/.bashrc
+
+echo alias debian='"cd $HOME ; proot-distro login debian"' >> $HOME/.bashrc
 
 exit
 
